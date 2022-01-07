3f635ae214cb6f1642149445cbe475d044805b27
Dustin Flynn <flynnd297@gmail.com>
2022-01-06 19:34:39 -0600

    Finished creating the classes for the game.

--- ex43.py
+++ ex43.py
@@ -109,26 +109,104 @@ class LaserWeaponArmory(Scene):
 class TheBridge(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+              You do a dive roll into the Weapon Armory, crouch and scan
+              the room for more Gothons that might be hiding.  It's dead
+              quiet, too quiet.  You stand up and run to the far side of
+              the room and find the neutron bomb in its container.
+              There's a keypad lock on the box and you need the code to
+              get the bomb out.  If you get the code wrong 10 times then
+              the lock closes forever and you can't get the bomb.  The
+              code is 3 digits.
+              """))
+
+        code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+        guess = input("[keypad]> ")
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print("BZZZZEDDD!")
+            guesses += 1
+            guess = input("[keypad]> ")
+
+        if guess == code:
+            print(dedent("""
+                  The container clicks open and the seal breaks, letting
+                  gas out.  You grab the neutron bomb and run as fast as
+                  you can to the bridge where you must place it in the
+                  right spot.
+                  """))
+            return 'the_bridge'
+        else:
+            print(dedent("""
+                  The lock buzzes one last time and then you hear a
+                  sickening melting sound as the mechanism is fused
+                  together.  You decide to sit there, and finally the
+                  Gothons blow up the ship from their ship and you die.
+                  """))
+            return 'death'
 
 class EscapePod(Scene):
+    
+    def enter(self):
+        print(dedent("""
+              You rush through the ship desperately trying to make it to
+              the escape pod before the whole ship explodes.  It seems
+              like hardly any Gothons are on the ship, so your run is
+              clear of interference.  You get to the chamber with the
+              escape pods, and now need to pick one to take.  Some of
+              them could be damaged but you don't have time to look.
+              There's 5 pods, which one do you take?
+              """))
+
+        good_pod = randint(1,5)
+        guess = input("[pod #]> ")
+
+
+        if int(guess) != good_pod:
+            print(dedent("""
+                  You jump into pod {guess} and hit the eject button.
+                  The pod escapes out into the void of space, then
+                  implodes as the hull ruptures, crushing your body into
+                  jam jelly.
+                  """))
+            return 'death'
+        else:
+            print(dedent("""
+                  You jump into pod {guess} and hit the eject button.
+                  The pod easily slides out into space heading to the
+                  planet below.  As it flies to the planet, you look
+                  back and see your ship implode then explode like a
+                  bright star, taking out the Gothon ship at the same
+                  time.  You won!
+                  """))
+
+            return 'finished'
+
+class Finished(Scene):
 
     def enter(self):
-        pass
+        print("You won! Good job.")
+        return 'finished'
 
 
 class Map(object):
+    
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
+    }
 
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
-
-
-a_map = Map('central_corridor')
-a_game = Engine(a_map)
-a_game.play()
+        return self.next_scene(self.start_scene)
